f38d828eb90a7f565d8a8b9574332c76f791bc75
jck-bit <jckkinyanjui@gmail.com>
2024-04-27 20:40:24 +0300

    added description-font
    changed body font style

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -42,7 +42,7 @@ export const Home = () => {
                     }}
                   />
                 </h1>
-                <p className="mb-1x">{introdata.description}</p>
+                <p className="mb-1x" style={{fontSize:"10.5px"}}>{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_h" className="ac_btn btn mb-2">
